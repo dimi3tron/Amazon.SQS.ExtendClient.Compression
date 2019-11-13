3c1277ed2b33edcb9abc8d9d477ad13d9e7e338f
Dimitri Troncquo <dimitri.troncquo@gmail.com>
2019-11-13 03:02:29 +0100

    add tests for CompressingClientConfiguration

---
+++ Amazon.SQS.ExtendClient.Compression.Test/CompressingClientConfigurationTests.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.SQS.ExtendClient.Compression.Test.Extensions;
+ using NUnit.Framework;
+ 
+ namespace Amazon.SQS.ExtendClient.Compression.Test
+ {
+     public class CompressingClientConfigurationTests
+     {
+         [Test]
+         public void Initialize_ObjectFromEmptyCtorAndObjectFromOther_AreEquivalent()
+         {
+             var subject1 = new CompressingClientConfiguration();
+             var subject2 = new CompressingClientConfiguration(subject1);
+
+             Assert.AreEqual(subject1.ToJson(), subject2.ToJson());
+         }
+     }
+ }
