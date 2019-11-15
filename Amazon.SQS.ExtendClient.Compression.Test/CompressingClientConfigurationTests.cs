ee53c1572ac7da296f29cb75eabc08b7d3a10217
Dimitri Troncquo <dimitri.troncquo@gmail.com>
2019-11-15 10:53:28 +0100

    add CompressingClientConfiguration tests

--- Amazon.SQS.ExtendClient.Compression.Test/CompressingClientConfigurationTests.cs
+++ Amazon.SQS.ExtendClient.Compression.Test/CompressingClientConfigurationTests.cs
@@ -1,17 +1,107 @@
 ﻿using Amazon.SQS.ExtendClient.Compression.Test.Extensions;
- using NUnit.Framework;
- 
- namespace Amazon.SQS.ExtendClient.Compression.Test
- {
-     public class CompressingClientConfigurationTests
-     {
-         [Test]
-         public void Initialize_ObjectFromEmptyCtorAndObjectFromOther_AreEquivalent()
-         {
-             var subject1 = new CompressingClientConfiguration();
-             var subject2 = new CompressingClientConfiguration(subject1);
+using NUnit.Framework;
 
-             Assert.AreEqual(subject1.ToJson(), subject2.ToJson());
-         }
-     }
- }
+namespace Amazon.SQS.ExtendClient.Compression.Test
+{
+    public class CompressingClientConfigurationTests
+    {
+        [Test]
+        public void Initialize_ObjectFromEmptyCtorAndObjectFromOther_AreEquivalent()
+        {
+            var subject = new CompressingClientConfiguration();
+            var result = new CompressingClientConfiguration(subject);
+
+            Assert.AreEqual(subject.ToJson(), result.ToJson());
+        }
+
+        [Test]
+        public void WithCompressionLevel_AnyValidCompressionLevel_ReturnsSame()
+        {
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithCompressionLevel(CompressionLevel.High);
+
+            Assert.AreEqual(subject, result);
+        }
+
+        [Test]
+        public void WithCompressionSizeThreshold_AnyValidLong_ReturnsSame()
+        {
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithCompressionSizeThreshold(1000);
+
+            Assert.AreSame(subject, result);
+        }
+
+        [Test]
+        public void WithAlwaysCompress_AnyValidBool_ReturnsSame()
+        {
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithAlwaysCompress(true);
+
+            Assert.AreSame(subject, result);
+        }
+
+        [Test]
+        public void WithCompressionLevel_AnyValidCompressionLevel_SetsCompressionLevel()
+        {
+            var value = CompressionLevel.High;
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithCompressionLevel(value).CompressionLevel;
+
+            Assert.AreEqual(value, result);
+        }
+
+        [Test]
+        public void WithCompressionLevel_AnyValidCompressionLevel_SetsCompressionLeveOnCompressionProvider()
+        {
+            var value = CompressionLevel.High;
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithCompressionLevel(value)
+                .CompressionProvider.CompressionLevel;
+
+            Assert.AreEqual(value, result);
+        }
+
+        [Test]
+        public void WithCompressionSizeThreshold_AnyValidLong_SetsCompressionSizeThreshold()
+        {
+            var value = 1000;
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithCompressionSizeThreshold(value)
+                .CompressionSizeThreshold;
+
+            Assert.AreEqual(value, result);
+        }
+
+        [Test]
+        public void WithAlwaysCompress_AnyValidBool_SetsAlwaysCompress()
+        {
+            var value = true;
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithAlwaysCompress(value)
+                                 .AlwaysCompress;
+
+            Assert.AreEqual(value, result);
+        }
+        
+        [Test]
+        public void WithAlwaysCompress_True_MessageParserIsInstanceOfImplicitCompressionMessageParser()
+        {
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithAlwaysCompress(true)
+                                .MessageParser;
+
+            Assert.IsInstanceOf<ImplicitCompressionMessageParser>(result);
+        }
+        
+        [Test]
+        public void WithAlwaysCompress_False_MessageParserIsInstanceOfDefaultMessageParser()
+        {
+            var subject = new CompressingClientConfiguration();
+            var result = subject.WithAlwaysCompress(false)
+                .MessageParser;
+
+            Assert.IsInstanceOf<DefaultMessageParser>(result);
+        }
+    }
+}
